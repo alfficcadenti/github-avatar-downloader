e5eba5d42ff2382d5105d049368e48bacb72572b
alfficcadenti <alfredoficcadenti@gmail.com>
2019-01-15 00:44:40 +0000

    missing input error included

--- download_avatars.js
+++ download_avatars.js
@@ -2,10 +2,13 @@ var request = require('request');
 var secret = require('./secret.js');
 
 const gitHubKey = "token " + secret.GITHUB_TOKEN;
+var repoOwner = process.argv[2];
+var repoName = process.argv[3];
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
+
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
@@ -25,20 +28,18 @@ function downloadImageByURL(url, filePath) {
 }
 
 
-var repoOwner = process.argv[2];
-var repoName = process.argv[3];
-
-
-getRepoContributors(repoOwner, repoName, function(err, result) {
-  console.log("Errors:", err);
-  //console.log("Result:", result);
-  var contributors = JSON.parse(result);
-  contributors.forEach(function(contrib) {
-    var file_path = "avatars/" + contrib.login + ".jpg";
-    var url = contrib.avatar_url;
-    downloadImageByURL(url, file_path);
+if (repoName != undefined && repoOwner != undefined) {
+  getRepoContributors(repoOwner, repoName, function(err, result) {
+    console.log("Errors:", err);
+    //console.log("Result:", result);
+    var contributors = JSON.parse(result);
+    contributors.forEach(function(contrib) {
+      var file_path = "avatars/" + contrib.login + ".jpg";
+      var url = contrib.avatar_url;
+      downloadImageByURL(url, file_path);
+    });
   });
-});
-
+}
+else {console.log("Error: Missing input!")}
 
 
